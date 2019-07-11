66c4f21331221f2ca1934089c73c6f24612ae5f9
tamiwo <tamiwo.otutama@gmail.com>
2019-07-11 23:56:50 +0900

    Add: FixedTimer

---
+++ Sample2D/Assets/Otutama/Timer/FixedTimer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Otutama.Timer
+{
+    public class FixedTimer : MonoBehaviour
+    {
+        [SerializeField]
+        private float interval = default; // 実行間隔
+        [SerializeField]
+        private bool callAtStart = false; // Start時にイベントを実行するかどうか
+        [SerializeField]
+        private UnityEvent events = new UnityEvent(); // 実行するイベント
+
+        private float time;
+
+        private void Start()
+        {
+            ResetTimer();
+            if (callAtStart == true)
+            {
+                events.Invoke();
+            }
+        }
+
+        private void Update()
+        {
+            time += Time.deltaTime;
+            if (time >= interval)
+            {
+                events.Invoke();
+                ResetTimer();
+            }
+        }
+
+        private void ResetTimer()
+        {
+            time = 0f;
+        }
+    }
+}
