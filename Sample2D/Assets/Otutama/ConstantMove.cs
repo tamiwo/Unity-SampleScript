de5ea8117d237aa4eb09d35ab19f9f7e9a80ebb2
R.Nishimura <ryota.nishimura@uquest.co.jp>
2019-08-20 12:38:15 +0900

    Add: ConstantMove

---
+++ Sample2D/Assets/Otutama/ConstantMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantMove : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 speed = default;
+
+    private void Update()
+    {
+        transform.position += speed * Time.deltaTime;
+    }
+
+}
